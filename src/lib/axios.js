3c644497bc86be87943adac8a6e1ca92b2f75910
nojuskybartas <nojus.kybartas@gmail.com>
2022-01-16 22:31:45 +0100

    updated links

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'http://localhost:5001/clone-8841d/us-central1/api'
-  // 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
-    // "http://localhost:5001/challenge-4b2b2/us-central1/api",
+  baseURL: 'https://us-central1-clone-8841d.cloudfunctions.net/api'
+  // 'http://localhost:5001/clone-8841d/us-central1/api'
 });
 
 export default instance;
